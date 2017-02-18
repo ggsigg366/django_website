dfdc0699a1aa56b56930099f9d8197d981c18341
Gerardo Garcia Sigg <ggsigg@gmail.com>
2017-02-18 14:36:04 -0600

    Finish first part

--- music/urls.py
+++ music/urls.py
@@ -5,11 +5,19 @@ app_name = 'music'
 
 urlpatterns = [
     #/music/
-    url(r'^$', views.index, name='index'),
+    #url(r'^$', views.index, name='index'),
 
     #/music/id/
-    url(r'^(?P<album_id>[0-9]+)/$', views.detail, name='detail'),
+    #url(r'^(?P<album_id>[0-9]+)/$', views.detail, name='detail'),
+# With generic views
+	#/music/
+    url(r'^$', views.IndexView.as_view(), name='index'),
+    url(r'^register/$', views.UserFormView.as_view(), name='register'),
+    url(r'^(?P<pk>[0-9]+)/$', views.DetailView.as_view(), name='detail'),
 
-    #/music/id/favorite/
-    url(r'^(?P<album_id>[0-9]+)/favorite/$', views.favorite, name='favorite'),
+    url(r'album/add/$', views.AlbumCreate.as_view(), name='album-add'),
+
+    url(r'album/(?P<pk>[0-9]+)/$', views.AlbumUpdate.as_view(), name='album-update'),
+
+    url(r'album/(?P<pk>[0-9]+)/delete/$', views.AlbumDelete.as_view(), name='album-delete'),
 ]
